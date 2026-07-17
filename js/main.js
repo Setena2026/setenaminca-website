efb1b39a3f0d01bf5c670382d8e502a2fbd4b2ab
Setena2026 <se.tenaminca@gmail.com>
2026-07-17 07:16:42 -0500

    Refactor menu toggle and add lightbox functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,64 @@
-const button = document.getElementById("menu-toggle");
+// MENU
 
+const button = document.getElementById("menu-toggle");
 const menu = document.querySelector(".nav-center");
 
-button.addEventListener("click", function(){
+if(button){
 
-    menu.classList.toggle("active");
+    button.addEventListener("click",function(){
+
+        menu.classList.toggle("active");
+
+    });
+
+}
+
+
+// LIGHTBOX
+
+const galleryImages=document.querySelectorAll(".gallery-grid img");
+
+const lightbox=document.getElementById("lightbox");
+
+const lightboxImg=document.getElementById("lightbox-img");
+
+const close=document.querySelector(".close-lightbox");
+
+
+galleryImages.forEach(img=>{
+
+    img.addEventListener("click",()=>{
+
+        lightbox.classList.add("active");
+
+        lightboxImg.src=img.src;
+
+    });
 
 });
+
+
+if(close){
+
+    close.addEventListener("click",()=>{
+
+        lightbox.classList.remove("active");
+
+    });
+
+}
+
+
+if(lightbox){
+
+    lightbox.addEventListener("click",(e)=>{
+
+        if(e.target===lightbox){
+
+            lightbox.classList.remove("active");
+
+        }
+
+    });
+
+}
